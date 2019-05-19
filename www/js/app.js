ed9176440dca5d6b4edd90670938c15d9aca301a
gustavocorreia <gustavocorreiasantos@outlook.com>
2019-05-19 02:51:32 -0300

    login firebase

---
+++ www/js/app.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyCVPUdC5qCv1-gd_fzn672kXtu648XvadY",
+    authDomain: "fiap-17mob-project.firebaseapp.com",
+    databaseURL: "https://fiap-17mob-project.firebaseio.com",
+    projectId: "fiap-17mob-project",
+    storageBucket: "fiap-17mob-project.appspot.com",
+    messagingSenderId: "743540642688",
+    appId: "1:743540642688:web:089e871a0dcf3f86"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
---
+++ www/js/login.js
@@ -0,0 +1,31 @@
+var btnEntrar = document.getElementById('btnEntrar');
+
+btnEntrar.addEventListener('click', function(e){
+
+    let txtEmail = document.getElementById('txtEmail');
+    let txtSenha = document.getElementById('txtSenha');
+
+    if(txtEmail.value == ''){
+        txtEmail.focus();
+        return;
+    }
+
+    if(txtSenha.value == ''){
+        txtSenha.focus();
+        return;
+    }
+
+    firebase.auth().signInWithEmailAndPassword(txtEmail.value, txtSenha.value)
+            .then(function (firebaseUser) {
+                window.location = "inicio.html" 
+            })
+            .catch(function (exception) {
+                if (exception.code === 'auth/wrong-password') {
+                    alert('Senha incorreta.');
+                    txtSenha.focus();
+                } else {
+                    alert(exception.message);
+                }
+            });
+
+});
